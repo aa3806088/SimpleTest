3381f82b02a1df9cc8fb1dd1b91b60f736c31bfb
ZJ <zhangjian394918141@163.com>
2025-02-15 18:18:37 +0800

    hello

---
+++ src/test/java/com/zhangjian/ideatest/IdeaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangjian.ideatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
